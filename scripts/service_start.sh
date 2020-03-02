bbb7fe46afa98fbe54bd19af82a25483426bede3
Winter Snowfall <WinterSnowfall@users.noreply.github.com>
2020-03-02 23:34:43 +0200

    Delete service_start.sh

--- scripts/service_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-nohup python3 pi_wrestled_service.py 1>/dev/null 2>&1 &
-sleep 2s
-echo "Service started - now tailing logs. Press CTRL + C to exit."
-tail -f ../logs/pi_wrestled_service.log
-
